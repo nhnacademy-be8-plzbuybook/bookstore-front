e6fb71e3ce765a7366737723a04bacebc0b213d8
jalyice <jalyice@gmail.com>
2024-12-11 15:19:06 +0900

    2 commit

--- src/main/java/com/nhnacademy/bookstorefront/main/controller/HomeController.java
+++ src/main/java/com/nhnacademy/bookstorefront/main/controller/HomeController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/")
     public String home() {
-        return "main/main";
+        return "main";
     }
 
 }
