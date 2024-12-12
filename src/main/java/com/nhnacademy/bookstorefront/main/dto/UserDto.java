9143fb9aed7c6a064f55b4ae632140c9e52b9fc9
choegwanghyeog <fkqlaus@naver.com>
2024-12-12 14:05:29 +0900

    프론트- 게이트웨이-인증서버 토큰발급 테스트

---
+++ src/main/java/com/nhnacademy/bookstorefront/main/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookstorefront.main.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+
+    String username;
+    String password;
+}
