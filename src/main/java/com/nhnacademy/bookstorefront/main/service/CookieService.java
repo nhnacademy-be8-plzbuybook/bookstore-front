a101e065b636a21ea3fa9a70e826f76936372f57
kkkkimtaehyeon <kth1927@naver.com>
2024-12-23 23:43:16 +0900

    쿠키 삭제 기능 구현

--- src/main/java/com/nhnacademy/bookstorefront/main/service/CookieService.java
+++ src/main/java/com/nhnacademy/bookstorefront/main/service/CookieService.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.bookstorefront.main.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface CookieService {
-    void addOnCookie(HttpServletResponse response,String key, String value, int expiry);
+    void addCookie(HttpServletResponse response, String key, String value, int expiry);
+    void removeCookie(HttpServletRequest request, HttpServletResponse response, String key);
 }
