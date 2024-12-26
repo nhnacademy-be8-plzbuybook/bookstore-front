0c766bd46bdfb2c04347c8fe9cd0bc9f2b851010
yunjiho <yoonwlgh12@gmail.com>
2024-12-26 20:33:46 +0900

    이메일을 통해 회원 정보를 가져오는 service 구현

--- src/main/java/com/nhnacademy/bookstorefront/main/service/AuthenticationService.java
+++ src/main/java/com/nhnacademy/bookstorefront/main/service/AuthenticationService.java
@@ -3,8 +3,10 @@ package com.nhnacademy.bookstorefront.main.service;
 import com.nhnacademy.bookstorefront.main.dto.LoginRequestDto;
 import com.nhnacademy.bookstorefront.main.dto.auth.LoginResponseDto;
 import com.nhnacademy.bookstorefront.main.dto.auth.OauthLoginResponseDto;
+import com.nhnacademy.bookstorefront.main.dto.mypage.MyPageDto;
 
 public interface AuthenticationService {
     LoginResponseDto processLogin(LoginRequestDto loginRequest);
     OauthLoginResponseDto processOauthLogin(String code);
+    MyPageDto getMyPage();
 }
