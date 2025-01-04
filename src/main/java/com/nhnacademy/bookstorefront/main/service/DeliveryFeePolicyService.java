0790e54d7c057ca2288394fba13872d4208274f2
kkkkimtaehyeon <kth1927@naver.com>
2025-01-05 03:07:45 +0900

    배송비정책 가져오는 거 구현

---
+++ src/main/java/com/nhnacademy/bookstorefront/main/service/DeliveryFeePolicyService.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.bookstorefront.main.service;
+
+import com.nhnacademy.bookstorefront.main.dto.DeliveryFeePolicyDto;
+
+public interface DeliveryFeePolicyService {
+    DeliveryFeePolicyDto getGeneralPolicy();
+}
