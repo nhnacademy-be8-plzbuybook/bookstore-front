4aed5fbca8b5905047c611043a7860153cc7b5be
yunjiho <yoonwlgh12@gmail.com>
2025-01-04 14:51:20 +0900

    쿠키에 있는 aceess토큰을 파싱해서 인증서버로 전송했습니다.

--- src/main/java/com/nhnacademy/bookstorefront/main/controller/IndexController.java
+++ src/main/java/com/nhnacademy/bookstorefront/main/controller/IndexController.java
@@ -5,6 +5,7 @@ import com.nhnacademy.bookstorefront.main.client.BookClient;
 import com.nhnacademy.bookstorefront.main.dto.BookDetailResponseDto;
 import com.nhnacademy.bookstorefront.main.dto.PagedResponse;
 import com.nhnacademy.bookstorefront.main.service.AuthenticationService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +23,7 @@ import java.util.List;
 public class IndexController {
     private final BookClient bookClient;
     private final AuthenticationService authenticationService;
+    private final AuthenticationClient authenticationClient;
 
     @GetMapping({"/index", "/"})
     public String index(
@@ -34,6 +36,15 @@ public class IndexController {
         Page<BookDetailResponseDto> response = bookClient.getBooks(page, size, sortBy, sortDir);
 
         boolean isLoggedIn = authenticationService.isLoggedIn(request);
+        String role = null;
+
+        if(isLoggedIn) {
+            String token = getTokenFromCookies(request);
+            if(token != null) {
+                role = authenticationClient.getRoleFromToken("Bearer " + token).getBody();
+            }
+        }
+
 
         model.addAttribute("books", response.getContent());
         model.addAttribute("currentPage", response.getNumber());
@@ -42,7 +53,19 @@ public class IndexController {
         model.addAttribute("sortDir", sortDir);
         model.addAttribute("pageSize", size);
         model.addAttribute("isLoggedIn", isLoggedIn);
+        model.addAttribute("role", role);
 
         return "index"; // index.html 렌더링
+    }
+
+    private String getTokenFromCookies(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("accessToken".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
     }
 }
